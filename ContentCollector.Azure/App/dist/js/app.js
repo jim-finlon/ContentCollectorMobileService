cdf3d878cdc0b27be04e8025d41f9341c897708a
jim-finlon <jfinlon1@gmail.com>
2016-10-04 13:04:26 -0600

    fix

--- ContentCollector.Azure/App/dist/js/app.js
+++ ContentCollector.Azure/App/dist/js/app.js
@@ -6,7 +6,7 @@
     angular.module('app').config(function (uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
             //key: 'get a key at https://console.developers.google.com'
-            key: 'AIzaSyAHOk2EXGXpnvVYxcUWVV4OFwomnnq2R0s'
+            key: 'AIzaSyChoRWbL0V1ouvBeAbeIfwq3a5DwiH8KKA'
             // v: '3.20', //defaults to latest 3.X anyhow
             // libraries: 'weather,geometry,visualization'
         });
